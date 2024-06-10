1c029e78bc3fb5856d3ae99a0ac04ed6f3050b34
safabeegum <pasafabeegum@gmail.com>
2024-06-10 21:17:27 +0530

    Completed

---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar/>
+        <div className="container">
+            <div className="row">
+                <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                <div class="card">
+                <img src="https://img.freepik.com/free-photo/happy-young-female-student-holding-notebooks-from-courses-smiling-camera-standing-spring-clothes-against-blue-background_1258-70161.jpg?t=st=1718033623~exp=1718037223~hmac=f7d2186c7fc5a79a5918aff8680cddeafe57db5370847dc49e2ce203abb0dd62&w=1060" class="card-img-top" alt="..."></img>
+  
+                </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
